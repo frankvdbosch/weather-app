d15469e08744db2b28fb1f4f490343d3e6d31788
frankvdbosch <frankvdbosch@hotmail.com>
2024-12-04 14:21:09 +0100

    Added perceived temperature to weather.blade.php

--- resources/views/weather.blade.php
+++ resources/views/weather.blade.php
@@ -7,5 +7,6 @@
         <h1>Current weather in {{ $weatherData['name'] }}</h1>
         <p>Description {{ $weatherData['weather'][0]['description']}}</p>
         <p>Temperature: {{ $weatherData['main']['temp'] }} &#8451;</p>
+        <p>Percieved temperature: {{ $weatherData['main']['feels_like'] }} &#8451;</p>
     </body>
 </html>
